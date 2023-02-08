9e1120c44e1711cf963e7224078d0e1a8802ad91
meepermad <atem.akuei@gmail.com>
2023-02-08 14:01:05 -0600

    Changing Main Location

--- src/main/java/frc/robot/robot/Main.java
+++ src/main/java/frc/robot/robot/Main.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.robot.ConstantsAndConfigs;
+package frc.robot.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.robot.RobotKeybindsAndFunctions.Robot;
