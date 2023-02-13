f01a6d7e8563074bd18cbbb80eb84c1014efa74c
meepermad <atem.akuei@gmail.com>
2023-02-13 11:22:17 -0600

    Adding Classes For Future Work

---
+++ src/main/java/frc/robot/robotCode/commands/Balance.java
@@ -0,0 +1,14 @@
+package frc.robot.robotCode.commands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.robotCode.ConstantsAndConfigs.Constants;
+import frc.robot.robotCode.subsystems.Swerve;
+
+public class Balance extends CommandBase{
+    
+}
